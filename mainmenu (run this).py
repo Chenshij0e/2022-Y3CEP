526a7207cf3279d2f86ae57cee1703782291930a
Chenshij0e <110016562+Chenshij0e@users.noreply.github.com>
2022-08-28 22:40:29 +0800

    slightly altered main menu

--- mainmenu (run this).py
+++ mainmenu (run this).py
@@ -8,27 +8,28 @@ print("\n----------------------------------------------------------------")
 print("                ♠♣♥♦ WELCOME TO BLACKJACK! ♠♣♥♦")
 print("                          MAIN MENU")
 print("----------------------------------------------------------------")
-print(
+
+    
+def main_menu(player):
+    print(
         "(1) Start new game\n(2) Check balance\n(3) Add balance"
         )
-    
-def main_menu():
     menu_input = input("What would you like to do? ")
     while menu_input not in ['1', '2', '3']:
-        menu_input = input("Invalid input, please choose a valid option. ")
+        menu_input = input("Invalid input, please choose a valid option: ")
     if menu_input == '1':
         newgame(player)
     elif menu_input == '2':
         print("Your balance is: " + str(player.check_value()))
         main_menu()
     elif menu_input == '3':
-        added_balance = float(input("How much do you want to add? "))
-        player.add_value(added_balance)
-        print(f"You have added {added_balance} to your balance!")
+        print("How much do you want to add? ")
+        while True:
+            try:
+                added_balance = float(input())
+                player.add_value(added_balance)
+                print(f"You have added {added_balance} to your balance!")
+                break
+            except:
+                print("Invalid input, please enter a number: ")
         main_menu()
-
-
-loop = 1     
-
-while loop == 1:
-    main_menu()

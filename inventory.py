33ff305d22728feca09a95f7ce767dcfcb2523ee
Chenshij0e <110016562+Chenshij0e@users.noreply.github.com>
2022-09-09 21:56:04 +0800

    final version

--- inventory.py
+++ inventory.py
@@ -3,10 +3,16 @@ class Inventory:
         self.value = value
 
     def add_value(self, adding):
+        #adds to balance
         self.value += adding
 
     def place_bet(self, bet):
+        #removes from balance
         self.value -= bet
 
     def check_value(self):
+        #returns balance
+        return self.value
+
+    def __str__(self):
         return self.value
